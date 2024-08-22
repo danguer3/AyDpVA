d44d8e2d1d1bdd4e6cd6044ff7c8c6ee8471efd9
manolo4413 <carsop96@gmail.com>
2024-08-22 17:41:57 -0600

    practica1.1.py

--- tasks/example.py
+++ tasks/example.py
@@ -1,1 +1,20 @@
-Print("Hello word")
+#Este es un programa de ejemplo
+def suma(num1,num2):
+    sum= num1 + num2
+    return sum
+def main():
+    x= int(input("Dame el primer numero: "))
+    y= int(input("Dame el segundo numero: "))
+    sumFinal=suma(x,y)
+    print("la suma es: " + str(sumFinal))
+
+x = 4
+y = 4
+if x > y: 
+        print("Hello Word")
+elif x ==y: 
+        print("Bye Bye")
+else:
+        print("Im happy")
+if __name__ == "__main__":
+    main()
--- tasks/practica1.1.py
+++ tasks/practica1.1.py
@@ -1,7 +1,9 @@
 ##PRACTICA 1.1 
-##Algoritmos y dispositivos de vehiculos antonomos
-## CALCULADORA PYTHON 
+##Algoritmos y dispositivos para Vehiculos Antonomos
+## CALCULADORA 
 #EMMANUEL PEREZ RANGEL
+import math
+
 def suma(num1,num2):
     sum=num1+num2
     return sum
@@ -16,7 +18,7 @@ def multiplicacion(num1,num2):
 
 def division(num1,num2):
     if num2==0:
-         print("No es posible la division")
+         print("No es posible la division entre 0")
     else:
         divid=num1/num2
     return divid
@@ -29,7 +31,6 @@ def modulo(num1,num2):
     mod=num1 % num2 
     return mod  
 
-import math
 def raiz(num1):
     if num1<0:
        print("No es posible calcular la raiz de un numero negativo")
@@ -38,43 +39,45 @@ def raiz(num1):
     return rz 
 
 def calculadora ():
+    while(True):
+        print("Operaciones Disponibles")
+        print("1. Suma ")
+        print("2. Resta ") 
+        print("3. Multiplicacion ")
+        print("4. Division")
+        print("5. Modulo")
+        print("6. Potencia")
+        print("7. Raiz cuadrada")
+        print("8. Salir")
 
-    print("Operaciones aritmeticas")
-    print("1. suma ")
-    print("2. resta ") 
-    print("3. multiplicacion ")
-    print("4. Division")
-    print("5. Modulo")
-    print("6. Potencia")
-    print("7. Raiz cuadrada")
+        operator= int(input("Ingrese la operacion que desea realizar: "))
+        x= float(input("Ingrese el primer numero: "))
+        y= float(input("Ingrese el segundo numero: "))
 
-operator= int(input("Ingrese la operacion que desea realizar: "))
-x= float(input("Dame el primer numero: "))
-y= float(input("Dame el segundo numero: "))
+        if operator==1:
+                SumFinal= suma(x,y)
+                print("Resultado: " + str(SumFinal))
+        elif operator==2: 
+                ResFinal=resta(x,y)
+                print("Resultado: "+ str(ResFinal))
+        elif operator==3:
+            MultiFinal= multiplicacion(x,y)
+            print("Resultado:" + str(MultiFinal))
+        elif operator==4:
+            DivFinal= division(x,y)
+            print("Resultado: "+ str(DivFinal))
+        elif operator ==5: 
+            ModFinal= modulo(x,y)
+            print("Modulo: "+ str(ModFinal))
+        elif operator ==6:
+            PotFinal=potencia(x,y)
+            print("Resultado: " + str(PotFinal))
+        elif operator==7:
+            RaizFinal=raiz(x)
+            print("resultado: " + str(RaizFinal))
+        else:
+            break
 
-def operations(operator, x,y):
-
-    if operator==1:
-        SumFinal= suma(x,y)
-        print("Resultado: " + (SumFinal))
-    elif operator==2: 
-        ResFinal=resta(x,y)
-        print("Resultado: "+ (ResFinal))
-    elif operator==3:
-       MultiFinal= multiplicacion(x,y)
-       print("Resultado:" + str(MultiFinal))
-    elif operator==4:
-       DivFinal= division(x,y)
-       print("Resultado: "+ str(DivFinal))
-    elif operator ==5: 
-     ModFinal= modulo(x,y)
-     print("Modulo: "+ str(ModFinal))
-    elif operator ==6:
-     PotFinal=potencia(x,y)
-     print("Resultado: " + str(PotFinal))
-    elif operator==7:
-     RaizFinal=raiz(x)
-     print("resultado: " + str(RaizFinal))
      
 
 if __name__ == "__main__":
